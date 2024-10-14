ab69eb9c665ba0fb36cc621c04386b97f28f75be
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-10-14 18:14:10 +0200

    destroy obs

--- src/features/todo/services/todo-service.service.ts
+++ src/features/todo/services/todo-service.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {map, Observable, take} from 'rxjs';
 import {Todo} from '../models/todo';
 
 @Injectable({
@@ -11,20 +11,20 @@ export class TodoService{
   private  url = 'https://jsonplaceholder.typicode.com/todos'
 
   getAllTodo() : Observable<Todo[]> {
-    return this.httpClient.get<Todo[]>(this.url).pipe(map(tods => tods.slice(0,10))
+    return this.httpClient.get<Todo[]>(this.url).pipe(take(1),map(tods => tods.slice(0,10))
     );
   }
   getTodoById(id:string){
-    return this.httpClient.get<Todo>(`${this.url}/${id}`)
+    return this.httpClient.get<Todo>(`${this.url}/${id}`).pipe(take(1))
   }
 
   deleteTodoById (id:string) : Observable<Todo>{
-    return this.httpClient.delete<Todo>(`${this.url}/${id}`)
+    return this.httpClient.delete<Todo>(`${this.url}/${id}`).pipe(take(1))
   }
   createTodo(todo : Todo) : Observable<Todo>{
-    return this.httpClient.post<Todo>(this.url,todo)
+    return this.httpClient.post<Todo>(this.url,todo).pipe(take(1))
   }
   updateTodoById(id:string,todo:Todo) : Observable<Todo>{
-    return this.httpClient.put<Todo>(`${this.url}/${id}`,todo)
+    return this.httpClient.put<Todo>(`${this.url}/${id}`,todo).pipe(take(1))
   }
 }
